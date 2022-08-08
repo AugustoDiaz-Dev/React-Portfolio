77a6ea377caf842e414a969b3d1b11015532567f
Augusto <augustodiaz_1@hotmail.com>
2022-08-09 01:52:10 +1000

    Some projects added

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
-import { BsGithub } from "react-icons/bs";
+import { BsCodeSlash } from "react-icons/bs";
 
 function ProjectCards(props) {
   return (
@@ -14,8 +14,8 @@ function ProjectCards(props) {
           {props.description}
         </Card.Text>
         <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
+          <CgWebsite /> &nbsp;
+          {props.isBlog ? "Builder" : "Code"}
         </Button>
         {"\n"}
         {"\n"}
@@ -29,7 +29,7 @@ function ProjectCards(props) {
             target="_blank"
             style={{ marginLeft: "10px" }}
           >
-            <CgWebsite /> &nbsp;
+            <BsCodeSlash /> &nbsp;
             {"Demo"}
           </Button>
         )}
--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,16 +2,15 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/ResumeAugustoDiaz.pdf";
+import pdf from "../../Assets/ResumeAugustoDiaz.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  // "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
-  "https://drive.google.com/file/d/1URirjrXwB0Ojw_RI7YmURXGFSXu989Ik/view?usp=sharing";
-
+const resumeLink = 
+"https://raw.githubusercontent.com/AugustoDiaz-Dev/React-Portfolio/master/src/Assets/ResumeAugustoDiaz.pdf";
+  
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
 
